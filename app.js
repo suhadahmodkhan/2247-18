2a96e3bf4a9efe8b96024a1bb05fa44cbc6f45ac
suhadahmodkhan <suhadahmodkhan04@gmail.com>
2024-09-18 01:08:35 -0700

    ok

---
+++ app.js
@@ -0,0 +1,10 @@
+// node.js download 
+// node.js install
+// node -v check in command prompt
+// create a file of js ( like : app.js)
+// console.log("hello world")
+// command in terminal : node app.js
+
+console.log("hello world")
+console.log(7)
+console.log(true)
